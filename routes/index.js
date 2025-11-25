135f57ce93cbef20914358fbc0760a65957e2773
varshith eturu <varshitheturu@gmail.com>
2025-11-25 18:57:44 +0530

    Books Create/Index/New

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router();
+const Book = require('../models/book')
 
-router.get('/', (req, res) =>{
-    res.render('index')
+router.get('/', async(req, res) =>{
+    let recentBooks 
+    try{
+        recentBooks = await Book.find().sort({createdDate : 'desc'}).limit(10).exec()
+        
+    }catch{
+        recentBooks = []
+    }
+    res.render('index',{books : recentBooks})
+    
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 app.use(bodyParser.urlencoded({limit : '10mb', extended : false}))
 
 app.set('view engine', 'ejs')
@@ -20,6 +21,7 @@ app.use(express.static('public'))
 
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
